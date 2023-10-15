27a9721589e8b5af475d855e5657f427aecf9e68
AbdullahEsha <shahariaresha@gmail.com>
2023-10-15 16:22:47 +0600

    update token

--- components/CancelStatus.tsx
+++ components/CancelStatus.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const CancelStatus = () => {
   return (
     <span className="pb-1 pt-[2px] px-3 bg-red-500 text-white rounded-md w-[6.3rem] flex justify-center">
@@ -7,5 +6,4 @@ const CancelStatus = () => {
     </span>
   );
 };
-
 export default CancelStatus;
