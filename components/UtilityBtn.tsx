4d421619bd1357742f644c11ad9a61cee5c3e670
AbdullahEsha <shahariaresha@gmail.com>
2023-10-15 16:14:11 +0600

    update

--- components/UtilityBtn.tsx
+++ components/UtilityBtn.tsx
@@ -1,10 +1,8 @@
 import { ReactNode } from "react";
-
 interface PrimaryButtonProps {
   name: string;
   icon: ReactNode;
 }
-
 const UtilityBtn: React.FC<PrimaryButtonProps> = ({ name, icon }) => {
   return (
     <button className="bg-secondary py-1 px-4 rounded-md text-white flex gap-1 items-center text-[14px]">
@@ -13,5 +11,4 @@ const UtilityBtn: React.FC<PrimaryButtonProps> = ({ name, icon }) => {
     </button>
   );
 };
-
 export default UtilityBtn;
